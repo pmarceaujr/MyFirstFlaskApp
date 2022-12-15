e9ab9d653fce80a917c108bb0b4a42e3fd0db15a
Paul Marceau <paul.marceau@takeda.com>
2022-12-14 23:07:39 -0500

    config files and updated gitignore

---
+++ Refactored/BookApp/config/dev.py
@@ -0,0 +1,7 @@
+
+# dev config settings
+
+DEBUG = True
+SECRET_KEY = 'password'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
